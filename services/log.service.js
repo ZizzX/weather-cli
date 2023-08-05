0d9ec6aec73bc27a7d159b34f997aec3b1646a36
a.isapov <a.isapov@trafficwave.kz>
2023-08-05 14:52:25 +0600

    Завершение упражнения. Вывод погоды, сохранение города.

--- services/log.service.js
+++ services/log.service.js
@@ -20,8 +20,20 @@ const printHelp = () => {
     );
 }
 
+const printWeather = (res, icon) => {
+    console.log(
+        dedent`${chalk.bgCyanBright(' WEATHER ')} Погода в городе ${res.name}
+        ${icon} ${res.weather[0].description}
+        Температура: ${res.main.temp}° (ощущается как ${res.main.feels_like}°)
+        Влажность: ${res.main.humidity}%
+        Скорость ветра: ${res.wind.speed}км/ч
+        `
+    );
+}
+
 export {
     printError,
     printSuccess,
     printHelp,
+    printWeather
 }
--- weather.js
+++ weather.js
@@ -5,9 +5,9 @@
 // чтобы указать путь к интерпретатору,
 // который должен использоваться для запуска данного скрипта.
 import { getArgs } from './helpers/args.js';
-import {printHelp, printSuccess, printError} from "./services/log.service.js";
-import {saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
-import {getWeather} from "./services/api.service.js";
+import {printHelp, printSuccess, printError, printWeather} from "./services/log.service.js";
+import {getKeyValue, saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
+import {getIcon, getWeather} from "./services/api.service.js";
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -22,22 +22,54 @@ const saveToken = async (token) => {
     }
 }
 
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('Не передан город!');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('Город сохранен!');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForecast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon));
+    } catch (e) {
+        if (e?.response?.status === 404) {
+            printError('Не верно указан город');
+        } else if (e?.response?.status === 401) {
+            printError('Не верно указан токен');
+        } else {
+            printError(e?.message);
+        }
+    }
+}
+
 const initCLI = () => {
     const args = getArgs(process.argv);
+    // Переменные окружения
+    // console.log(process.env);
 
     if (args.h) {
-        printHelp();
+       return printHelp();
     }
     if (args.s) {
         // Сохранить город
+        return saveCity(args.s);
     }
     if (args.t) {
-        return saveToken(args.t);
         // Сохранить токен
+        return saveToken(args.t);
     }
 
-    return getWeather('moscow');
     // Вывести погоду
+    return getForecast();
 }
 
 initCLI();
